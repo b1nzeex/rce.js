0461b87ca42fff3e6cba7f3754a3f5966a2fc7c3
b1nzeex <cbeachx@gmail.com>
2024-07-23 22:52:46 +0100

    .

--- src/classes/RCEManager.ts
+++ src/classes/RCEManager.ts
@@ -61,6 +61,9 @@ export default class RCEManager extends RCEEvents {
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     );
+    await page.setExtraHTTPHeaders({
+      "Accept-Language": "en",
+    });
     await page.goto(GPORTALRoutes.HOME);
     await page
       .locator(
